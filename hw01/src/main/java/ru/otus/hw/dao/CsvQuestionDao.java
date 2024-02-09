225fea59941ac52218f14940160a8be9fe2890c4
Sergey <s.a1ekseevv16@gmail.com>
2024-02-09 13:01:47 +0300

    сконфигурирован xml-контекст, реализован вывод вопросов и ответов из csv-файла

--- hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,14 +1,24 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
-import java.util.ArrayList;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+
+import static java.util.Objects.*;
 
 @RequiredArgsConstructor
 public class CsvQuestionDao implements QuestionDao {
+    private static final char SEPARATOR = ';';
+    private static final int SKIP_LINES = 1;
+
     private final TestFileNameProvider fileNameProvider;
 
     @Override
@@ -18,6 +28,28 @@ public class CsvQuestionDao implements QuestionDao {
         // Использовать QuestionReadException
         // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
 
-        return new ArrayList<>();
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        try (InputStream inputStream = requireNonNull(classLoader.getResourceAsStream(fileNameProvider.getTestFileName()));
+             InputStreamReader streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+             BufferedReader reader = new BufferedReader(streamReader)) {
+
+            return buildCsvToBeans(reader).stream()
+                    .map(QuestionDto::toDomainObject)
+                    .toList();
+
+        } catch (IOException e) {
+            throw new QuestionReadException("The file %s could not be read"
+                    .formatted(fileNameProvider.getTestFileName()), e);
+        }
+    }
+
+    private static CsvToBean<QuestionDto> buildCsvToBeans(Reader reader) {
+        return new CsvToBeanBuilder<QuestionDto>(reader)
+                .withSkipLines(SKIP_LINES)
+                .withIgnoreLeadingWhiteSpace(true)
+                .withSeparator(SEPARATOR)
+                .withType(QuestionDto.class)
+                .build();
     }
 }
