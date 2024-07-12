5cd830099d3d726dc87af4b6d35bd246743031c9
Sergey <s.a1ekseevv16@gmail.com>
2024-07-12 13:39:02 +0300

    в docker-compose добавлена зависимость от контейнера с БД, в app.yml добавлено подключение к БД запущенной отдельно в контейнере

--- hw17/src/main/java/ru/otus/hw/Hw17Application.java
+++ hw17/src/main/java/ru/otus/hw/Hw17Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Hw10Application {
+public class Hw17Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Hw10Application.class, args);
+		SpringApplication.run(Hw17Application.class, args);
 		System.out.printf("Site: %n%s%n", "http://localhost:8080");
 	}
 
